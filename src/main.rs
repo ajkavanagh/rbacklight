32014eab8ed3a26f4ec3b6300bcbb739042f4660
Alex Kavanagh <alex@ajkavanagh.co.uk>
2018-08-21 07:21:51 +0100

    Clean up some of the functions to be a bit easier to read

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::env;
 use std::cmp;
 use std::process;
-use std::fs::File;
 use std::io::prelude::*;
 use std::fs::OpenOptions;
+use std::fs;
+
 
 // really want to build up the constants, but concat! only allows literals,
 // and const_concat! (crate) requires non-stable rust.
@@ -15,6 +16,7 @@ const DEFAULT_DELTA: u32 = 10;   // in percent
 const DEFAULT_SET: u32 = 70; // in percent
 
 const VALID_COMMANDS: &[&'static str] = &["get", "set", "inc", "dec", "help"];
+
 
 struct Config {
     command: String,
@@ -54,42 +56,38 @@ impl Config {
 
 /// Reads a u64 value from the sysfs path
 fn read_sysfs(path: &'static str) -> Result<u32, String> {
-    let f = File::open(path);
-    if f.is_err() {
-        return Err(format!("Couldn't open '{}' because '{}'", path, f.unwrap_err()));
+    match fs::read_to_string(path) {
+        Err(err) => Err(format!("Couldn't read from '{}' due to '{}'", path, err)),
+        Ok(s) => s.trim_right().parse::<u32>()
+            .and_then(|v| { Ok(v) })
+            .or_else(|err| { Err(format!("Couldn't parse value: '{}'", err)) }),
     }
-    let mut s = String::new();
-    if let Err(err) = f.unwrap().read_to_string(&mut s) {
-        return Err(format!("Couldn't read from '{}' because '{}'", path, err));
-    }
-    s.trim_right().parse::<u32>()
-        .and_then(|v| { Ok(v) })
-        .or_else(|err| { Err(format!("Couldn't parse value: '{}'", err)) })
 }
+
 
 /// Writes a u64 value to the sysfs path
 fn write_sysfs(path: &'static str, value: u32) -> Result<(), String> {
-    let f = OpenOptions::new().write(true).open(path);
-    if f.is_err() {
-        return Err(format!("Couldn't open '{}' because '{}'", path, f.unwrap_err()));
+    match OpenOptions::new().write(true).open(path) {
+        Err(err) => Err(format!("Couldn't open '{}' due to '{}'", path, err)),
+        Ok(mut f) => f.write_fmt(format_args!("{}", value))
+            .or_else(|err| {
+                Err(format!("Cound't write '{}' to '{}' because '{}'", value, path, err))
+            }),
     }
-    f.unwrap().write_fmt(format_args!("{}", value))
-        .or_else(|err| {
-            Err(format!("Cound't write '{}' to '{}' because '{}'", value, path, err))
-        })
 }
+
 
 fn maximum() -> Result<u32, String> {
     read_sysfs(MAX_BRIGHTNESS)
 }
 
+
 fn current() -> Result<u32, String> {
-    maximum()
-        .and_then(|max| {
-            read_sysfs(BRIGHTNESS)
-                .and_then(|v| { Ok(v *100 / max) })
-        })
+    maximum().and_then(|max| {
+        read_sysfs(BRIGHTNESS).and_then(|v| { Ok(v *100 / max) })
+    })
 }
+
 
 /// get the current backlight as a percentage
 fn get() -> Result<(), String> {
@@ -112,6 +110,7 @@ fn set(value: u32) -> Result<(), String> {
         .or_else(|err| { Err(err) })
 }
 
+
 /// increment the backlight as a percentage
 fn inc(value: u32) -> Result<(), String> {
     let delta = cmp::min(100, value);
@@ -119,6 +118,7 @@ fn inc(value: u32) -> Result<(), String> {
         .and_then(|c| { set(c + delta) })
         .or_else(|err| { Err(err) })
 }
+
 
 /// decrement the backlight as a percentage
 fn dec(value: u32) -> Result<(), String> {
@@ -151,7 +151,7 @@ fn main() {
         _ => usage(),
     };
     result.unwrap_or_else(|err| {
-        eprintln!("Could run command: '{}' because '{}'", config.command, err);
+        eprintln!("Couldn't run command: '{}' because '{}'", config.command, err);
         usage().unwrap();
         process::exit(1);
     });
